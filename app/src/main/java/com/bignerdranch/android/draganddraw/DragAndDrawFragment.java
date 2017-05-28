b2905929832977356793fe0cb8fa961474b17822
wanglianghai <1540446221@qq.com>
2017-05-28 19:01:14 +0800

    origin

---
+++ app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.draganddraw;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2017/5/28/028.
+ */
+
+public class DragAndDrawFragment extends Fragment {
+}
